98e8c5c6d971747046a235f07c309038f525624e
3-f-3 <francobuttice1@gmail.com>
2026-04-08 19:58:03 -0400

    Add files via upload

---
+++ Personal Branding Website/serve.js
@@ -0,0 +1,52 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+const ROOT = __dirname;
+
+const MIME = {
+  '.html': 'text/html',
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.jpeg': 'image/jpeg',
+  '.gif': 'image/gif',
+  '.svg': 'image/svg+xml',
+  '.ico': 'image/x-icon',
+  '.webp': 'image/webp',
+  '.woff': 'font/woff',
+  '.woff2': 'font/woff2',
+  '.ttf': 'font/ttf',
+};
+
+http.createServer((req, res) => {
+  let urlPath = req.url.split('?')[0];
+  if (urlPath === '/') urlPath = '/index.html';
+
+  const filePath = path.join(ROOT, urlPath);
+  const ext = path.extname(filePath);
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      // Try .html extension
+      const htmlPath = filePath + '.html';
+      fs.readFile(htmlPath, (err2, data2) => {
+        if (err2) {
+          res.writeHead(404, { 'Content-Type': 'text/plain' });
+          res.end('Not Found');
+        } else {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(data2);
+        }
+      });
+    } else {
+      res.writeHead(200, { 'Content-Type': MIME[ext] || 'application/octet-stream' });
+      res.end(data);
+    }
+  });
+}).listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
